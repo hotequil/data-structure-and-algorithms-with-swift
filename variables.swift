4a1019f1b96ec6ac101473a04716f12bc6dda6e6
João Paulo Hotequil <hotequil@protonmail.com>
2024-02-11 15:03:55 -0300

    feat: created variables.swift

---
+++ variables.swift
@@ -0,0 +1,23 @@
+var firstAge = 21
+var secondAge = 20
+
+/// This is a sum of ages
+var sumOfAges = firstAge + secondAge
+
+// Changing second age
+secondAge = 34
+secondAge += 8
+
+/*
+This is a big comment 
+with multiple lines
+*/
+print("secondAge: \(secondAge)")
+print("sumOfAges: \(sumOfAges)")
+
+// Constants are created with "let" reserverd word
+let text = "Hello, world"
+let phrase: String = "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+
+print(text)
+print(phrase)
